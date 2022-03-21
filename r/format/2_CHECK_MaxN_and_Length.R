c12c60175f26a56911d0838d27be708d72cc8a9c
Brooke Gibbons <brooke.gibbons@uwa.edu.au>
2022-03-21 11:12:28 +0800

    start to tidy up this script for molly

--- r/format/2_CHECK_MaxN_and_Length.R
+++ r/format/2_CHECK_MaxN_and_Length.R
@@ -1,4 +1,3 @@
-
 ### Error checks of MaxN and Length files created from EventMeasure or generic stereo-video annotations via GlobalArchive
 
 ## This script is designed to be used interatively to suggest species name and length corrections that should be made to original EventMeasure (.EMObs) or generic annotation files AND for subsequent data analysis.
@@ -38,55 +37,28 @@ library(ggplot2)
 
 ## Set Study Name ----
 # Change this to suit your study name. This will also be the prefix on your final saved files.
-study<-"project.example"
-
-## Folder Structure ----
-# This script uses one main folder ('working directory')
-
-# Three subfolders will already be created within the 'working directory'. They are 'Downloads','Data to be checked' and 'Tidy data' (Script 1)
-
-# In addition, this script will make new folders for:
-#'Plots' to save initial checking plots
-#'Errors to check' to save all the error files e.g. lists of taxa that are not in the life history sheet
-
-## Set your working directory ----
-working.dir<-dirname(rstudioapi::getActiveDocumentContext()$path) # sets working directory to that of this script - or type your own
-
-## Save these directory names to use later----
-to.be.checked.dir<-paste(working.dir,"Data to be checked",sep="/") 
-download.dir<-paste(working.dir,"Downloads",sep="/")
-tidy.dir<-paste(working.dir,"Tidy data",sep="/")
-plots.dir=paste(working.dir,"Plots",sep="/")
-error.dir=paste(working.dir,"Errors to check",sep="/")
-
-## Create a folder for Plots and Errors ----
-# The two lines below will create the 'Plots' and 'Errors to check' subfolders within the working directory
-dir.create(file.path(working.dir, "Plots"))
-dir.create(file.path(working.dir, "Errors to check"))
-
-# Import unchecked data from staging folder----
-setwd(to.be.checked.dir)
+study <- "add.name.here"
 
 # Import metadata ---
-metadata<-read.csv(paste(study,"metadata.csv",sep="_"))
+metadata <- read.csv(paste(study, "metadata.csv", sep="_"))
 
 # Import MaxN file---
-maxn<-read_csv(paste(study,"maxn.csv",sep="_"))%>%
-  mutate(maxn=as.numeric(maxn))%>%
-  mutate(species=tolower(species))%>%
-  select(campaignid,sample,family,genus,species,maxn)%>%
-  replace_na(list(family="Unknown",genus="Unknown",species="spp"))%>% # remove any NAs in taxa name
+maxn <- read_csv(paste(study, "maxn.csv", sep="_")) %>%
+  mutate(maxn = as.numeric(maxn)) %>%
+  mutate(species = tolower(species)) %>%
+  select(campaignid, sample, family, genus, species, maxn) %>%
+  replace_na(list(family = "Unknown", genus = "Unknown", species = "spp")) %>% # remove any NAs in taxa name
   glimpse()
 
 # Import length/3d file----
-length<-read_csv(file=paste(study,"length3dpoints.csv",sep = "_"),na = c("", " "))%>%
-  mutate(number=as.numeric(number))%>%
-  mutate(range=as.numeric(range))%>%
-  mutate(length=as.numeric(length))%>%
-  select(campaignid,sample,family,genus,species,length,number,range)%>%
+length<-read_csv(file = paste(study, "length3dpoints.csv", sep = "_"), na = c("", " ")) %>%
+  mutate(number = as.numeric(number)) %>%
+  mutate(range = as.numeric(range)) %>%
+  mutate(length = as.numeric(length)) %>%
+  select(campaignid, sample, family, genus, species, length, number, range) %>%
   filter(!is.na(number)) %>% # find and remove sync points that are not fish
-  replace_na(list(family="Unknown",genus="Unknown",species="spp"))%>% # remove any NAs in taxa name
-  mutate(genus=str_replace_all(.$genus,c("NA"="Unknown")))%>%
+  replace_na(list(family = "Unknown", genus = "Unknown", species = "spp")) %>% # remove any NAs in taxa name
+  mutate(genus = str_replace_all(.$genus,c("NA" = "Unknown"))) %>%
   glimpse()
 
 # BASIC checks----
@@ -94,19 +66,18 @@ length<-read_csv(file=paste(study,"length3dpoints.csv",sep = "_"),na = c("", " "
 # Use this check to identify any 3D points in your data. 3D points are often added if a length measurement couldn't be made.
 # Should you have 3D points? 
 
-three.d.points<-length%>%
-  filter(is.na(length))%>%
-  filter(!is.na(number))%>%
+three.d.points <- length %>%
+  filter(is.na(length)) %>%
+  filter(!is.na(number)) %>%
   glimpse() # Do we have 3d points? 
 
 # Check if we have more than one fish associated with single length measurement----
 # For large schools (e.g. 100+) a length measurement can be assigned to multiple individuals (by changing the 'Number' in EventMeasure). For example in a school of 500 if you measure 50 fish, and put '10' for each measurement in EM, you would have 500 fish but only 50 measurements
 # Use this check to see if you have these types of measurements (e.g. to fix a mistaken number against multiple lengths).
 
-schools<-length%>%
-  filter(number>1)%>%
+schools <- length %>%
+  filter(number>1) %>%
   glimpse() # Do we have schools? 
-
 
 # Plot to visualise length data ----
 # Add justification and units to x
@@ -117,31 +88,34 @@ theme_ga<-theme(panel.grid.major = element_blank(), panel.grid.minor = element_b
 
 # Length vs. Density ----
 # Use this plot to visualise the length distribution in your data
-ggplot(data=length, aes(as.numeric(length))) +
-  geom_histogram(aes(y =..density..),col="black",fill="grey",alpha = .5)+
-  scale_y_continuous(expand = expand_scale(mult = c(0, .1)))+
-  labs(x = "Length (mm)", y = "Density")+theme_ga
+ggplot(data = length, aes(as.numeric(length))) +
+  geom_histogram(aes(y = ..density..), col = "black", fill = "grey", alpha = .5) +
+  scale_y_continuous(expand = expand_scale(mult = c(0, .1))) +
+  labs(x = "Length (mm)", y = "Density") +
+  theme_ga
 
-ggsave(file=paste(study,"check.length.png",sep = "_"))
+ggsave(file = paste(study, "check.length.png", sep = "_"))
 
 # Range vs. Density ---
 # Use this plot to visulaise the range distribution in your data
-ggplot(data=length, aes(as.numeric(range))) +
-  geom_histogram(aes(y =..density..),col="black",fill="grey",alpha = .5)+
-  scale_y_continuous(expand = expand_scale(mult = c(0, .1)))+
-  labs(x = "Range (mm)", y = "Density")+theme_ga
+ggplot(data = length, aes(as.numeric(range))) +
+  geom_histogram(aes(y = ..density..), col = "black", fill = "grey", alpha = .5) +
+  scale_y_continuous(expand = expand_scale(mult = c(0, .1))) +
+  labs(x = "Range (mm)", y = "Density")+
+  theme_ga
 
-ggsave(file=paste(study,"check.range.png",sep = "_"))
+ggsave(file = paste(study, "check.range.png", sep = "_"))
 
 # Plot to visualise Range vs Length data ---
 # note that only 3Dpoints typically occur past 10m---
-ggplot(data=length, aes(range,length)) +
-  geom_point()+
-  geom_smooth()+
-  scale_y_continuous(expand = expand_scale(mult = c(0, .1)))+
-  labs(x = "Range (mm)", y = "Length (mm)")+theme_ga
+ggplot(data = length, aes(range, length)) +
+  geom_point() +
+  geom_smooth() +
+  scale_y_continuous(expand = expand_scale(mult = c(0, .1))) +
+  labs(x = "Range (mm)", y = "Length (mm)") +
+  theme_ga
 
-ggsave(file=paste(study,"check.range.vs.length.png",sep = "_"))
+ggsave(file = paste(study, "check.range.vs.length.png", sep = "_"))
 
 
 # Standardise for Range ----
@@ -150,14 +124,14 @@ ggsave(file=paste(study,"check.range.vs.length.png",sep = "_"))
 
 summary(length$range) # shows min, mean and max range
 
-out.of.range<-filter(length,range>10000)%>% # 10 m = 10000 mm
+out.of.range <- filter(length, range > 10000)%>% # 10 m = 10000 mm
   glimpse() # Shows fish more than 10 m away
 
 # SERIOUS data checks using the life.history googlesheet ----
 # Checks on fish length vs their max.length in the life.history sheet will be done below
 
 # life.history checks will:
-# 1. Check for species occurence vs their known distribution
+# 1. Check for species occurrence vs their known distribution
 # 2. Check for any species that may have changed names and suggest synonyms
 # 3. Check measured length vs max.length for that species
 
@@ -179,22 +153,23 @@ out.of.range<-filter(length,range>10000)%>% # 10 m = 10000 mm
 # Use the abbreviation in the code below
 # currently set for the Pilbara, Australia example data set ('NW' for North-west)
 
-master<-gs_title("Australia.life.history")%>%
-  gs_read_csv(ws = "australia.life.history")%>%ga.clean.names()%>%
-  filter(grepl('Australia', global.region))%>% # Change country here
-  filter(grepl('NW', marine.region))%>% # Select marine region (currently this is only for Australia)
-  dplyr::mutate(all=as.numeric(all))%>%
-  dplyr::mutate(bll=as.numeric(bll))%>%
-  dplyr::mutate(a=as.numeric(a))%>%
-  dplyr::mutate(b=as.numeric(b))%>%
-  select(family,genus,species,marine.region,length.measure,a,b,all,bll,fb.length_max,fb.ltypemaxm)%>%
-  distinct()%>%
+master <- gs_title("Australia.life.history") %>%
+  gs_read_csv(ws = "australia.life.history") %>%
+  ga.clean.names() %>%
+  filter(grepl('Australia', global.region)) %>% # Change country here
+  filter(grepl('NW', marine.region)) %>% # Select marine region (currently this is only for Australia)
+  dplyr::mutate(all=as.numeric(all)) %>%
+  dplyr::mutate(bll=as.numeric(bll)) %>%
+  dplyr::mutate(a=as.numeric(a)) %>%
+  dplyr::mutate(b=as.numeric(b)) %>%
+  select(family,genus,species,marine.region,length.measure,a,b,all,bll,fb.length_max,fb.ltypemaxm) %>%
+  distinct() %>%
   glimpse()
 
-synonyms <- gs_title("Synonyms_Australia")%>%
-  gs_read_csv(ws = "Synonyms_Australia")%>%
-  distinct()%>%
-  ga.clean.names()%>%
+synonyms <- gs_title("Synonyms_Australia") %>%
+  gs_read_csv(ws = "Synonyms_Australia") %>%
+  distinct() %>%
+  ga.clean.names() %>%
   select(-comment)
 
 # Update by synonyms ----
